c62eff4455befb3fd6ede52a54c2eac14f68823f
ThomasCostner37 <thomas31406@gmail.com>
2026-06-07 17:31:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,3 +6,4 @@
 const CLIENT_ID = '655217120371-j3vqj5umegsiqrqi7876g1lh2rpct77v.apps.googleusercontent.com';
 const DOC_ID = '10hZgYRG4TNxEtwXqt59MCsFqr8M1Y1MpgmhGVBqVZhw';
 const API_KEY = '';
+const ANTHROPIC_KEY = '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';
